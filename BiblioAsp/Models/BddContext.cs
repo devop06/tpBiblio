b02e1297730be45e92521cc4ca8723e8b357aa9f
Villanova <tonyvillanova@hotmail.fr>
2016-04-07 19:47:26 +0200

    Début de tp bibliothèque openclassroom asp.net

---
+++ BiblioAsp/Models/BddContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioAsp.Models
+{
+    public class BiblioContext : DbContext
+    {
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<Livre> Livres { get; set; }
+    }
+
+    public class BibilioInitializer : DropCreateDatabaseIfModelChanges<BiblioContext>
+    {
+    }
+}
---
+++ BiblioAsp/Models/Livre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioAsp.Models
+{
+    public class Livre
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public DateTime DateParution { get; set; }
+        [Required]
+        public virtual Auteur Auteur { get; set; }
+    }
+}
